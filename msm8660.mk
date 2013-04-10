9f49b87dd56b295028098b5fa321e5cdc589dc00
Dennis Bold <dennis@dennisbold.com>
2013-04-10 22:08:03 +0100

    Add fstab.pyramid for Sensation

--- msm8660.mk
+++ msm8660.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/qcom/common/common.mk)
 
-PRODUCT_NAME := msm8660_surf
-PRODUCT_DEVICE := msm8660_surf
+#PRODUCT_NAME := msm8660_surf
+#PRODUCT_DEVICE := msm8660_surf
 
 # Wallpapers
 PRODUCT_PACKAGES += Galaxy4
